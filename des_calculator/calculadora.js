84d9eb5784480c2c424c0a40c928c46e3c0d0428
dhicosta <dhiegofp14@gmail.com>
2023-07-20 18:16:07 -0300

    Segundo commit: Enviando novamente o desafio da calculadora

---
+++ des_calculator/calculadora.js
@@ -0,0 +1,24 @@
+let numero = ''
+let soma = 0
+function calculo(valor) {
+    numero += String(valor)
+    document.getElementById('valor').value = numero
+    console.log(numero)
+}
+function final() {
+    let resultado = document.getElementById('valor').value
+    if(resultado) {
+        document.getElementById('valor').value = eval(resultado)
+    } else {
+        document.getElementById('valor').value = "Nada a calcular"
+    }
+    
+}   
+function clean() {
+    numero = ""
+    document.getElementById('valor').value = numero
+}
+function back() {
+    numero = numero.substring(0, numero.length -1)
+    document.getElementById('valor').value = numero
+}
